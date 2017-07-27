e04cebe78d04054a9ddc203dafa09f86f53488af
Matthew Menna <mattmenna@gmail.com>
2017-07-27 09:59:23 -0400

    Initial Commit of Template

--- src/main/java/com/test/controller/HomeController.java
+++ src/main/java/com/test/controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.SQLException;
+
 @Controller
 public class HomeController {
+
+    @RequestMapping("/jdbc")
+
+    public String dbReturn(Model model) throws ClassNotFoundException, SQLException
+    {
+
+        return "jdbc";
+    }
 
     @RequestMapping("/")
 
@@ -15,5 +26,4 @@ public class HomeController {
                 ModelAndView("welcome","message","Hello World");
 
     }
-
 }
